d06544ee30555962b428725380450e845f336ee9
Thomas Vestergaard <mail@thomasvestergaard.com>
2013-12-27 20:18:46 +0100

    Added account info to GUI.
    Simple usercontrol and viewmodel to show account info.

--- BTCE-Trader/BTCE-Trader.UI/MainWindowViewModel.cs
+++ BTCE-Trader/BTCE-Trader.UI/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using BTCE_Trader.Api;
+using BTCE_Trader.Core.AccountInfo;
 using BTCE_Trader.Core.Depth;
 using BTCE_Trader.Core.Orders;
 using BTCE_Trader.UI.UI.UserControls;
@@ -10,6 +11,7 @@ namespace BTCE_Trader.UI
         private IDepthAgent depthAgent;
         private readonly IActiveOrderAgent activeOrderAgent;
         private readonly IBtceTradeApi btceTradeApi;
+        private readonly IAccountInfoAgent accountInfoAgent;
 
         public IDepthAgent DepthAgent
         {
@@ -19,15 +21,18 @@ namespace BTCE_Trader.UI
 
         public MarketDepthViewModel DepthViewModel { get; set; }
         public ActiveOrdersViewModel ActiveOrdersViewModel { get; set; }
+        public AccountInfoViewModel AccountInfoViewModel { get; set; }
 
-        public MainWindowViewModel(IDepthAgent depthAgent, IActiveOrderAgent activeOrderAgent, IBtceTradeApi btceTradeApi)
+        public MainWindowViewModel(IDepthAgent depthAgent, IActiveOrderAgent activeOrderAgent, IBtceTradeApi btceTradeApi, IAccountInfoAgent accountInfoAgent)
         {
             this.depthAgent = depthAgent;
             this.activeOrderAgent = activeOrderAgent;
             this.btceTradeApi = btceTradeApi;
+            this.accountInfoAgent = accountInfoAgent;
 
             DepthViewModel = new MarketDepthViewModel(depthAgent);
             ActiveOrdersViewModel = new ActiveOrdersViewModel(activeOrderAgent, btceTradeApi);
+            AccountInfoViewModel = new AccountInfoViewModel(accountInfoAgent);
         }
     }
 }
--- BTCE-Trader/BTCE-Trader.UI/UI/UserControls/AccountInfoViewModel.cs
+++ BTCE-Trader/BTCE-Trader.UI/UI/UserControls/AccountInfoViewModel.cs
@@ -1,9 +1,12 @@
-﻿using BTCE_Trader.Api.Info;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using BTCE_Trader.Api.Info;
 using BTCE_Trader.Core.AccountInfo;
+using BTCE_Trader.UI.Annotations;
 
 namespace BTCE_Trader.UI.UI.UserControls
 {
-    public class AccountInfoViewModel
+    public class AccountInfoViewModel : INotifyPropertyChanged
     {
         private readonly IAccountInfoAgent accountInfoAgent;
 
@@ -16,7 +19,23 @@ namespace BTCE_Trader.UI.UI.UserControls
         public AccountInfoViewModel(IAccountInfoAgent accountInfoAgent)
         {
             this.accountInfoAgent = accountInfoAgent;
+            this.accountInfoAgent.AccountUpdated += accountInfoAgent_AccountUpdated;
             
+        }
+
+        void accountInfoAgent_AccountUpdated(IAccountInfo accountInfo)
+        {
+            this.accountInfo = accountInfo;
+            OnPropertyChanged("AccountInfo");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
